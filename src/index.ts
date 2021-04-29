97392dd331b430dffe12ca5261fe505be950783f
Francesco Cioria <firefelix@gmail.com>
2021-04-29 16:41:41 +0200

    improve handler typings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import _lambda1 from "./lambda1";
 
+type Map = {
+  [k: string]: string;
+};
+
+type Event = {
+  resource: string;
+  path: string;
+  httpMethod: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  headers: Map;
+  queryStringParameters: null | Map;
+  pathParameters: null | Map;
+  stageVariables: null | Map;
+  requestContext: any;
+  body: string;
+  isBase64Encoded: boolean;
+};
+
 type Response = {
   statusCode: number;
   body: string;
 };
+
+type Handler = (event: Event) => Promise<Response>;
 
 const response = (body: any): Response => ({
   body: JSON.stringify(body),
@@ -17,7 +36,7 @@ const error = (error: string): Response => ({
   statusCode: 200
 });
 
-export const lambda1 = async (): Promise<Response> => {
+export const lambda1: Handler = async event => {
   try {
     const res = await _lambda1();
     return response(res);
